bb6641bac52cdfd0f89a860ceb73e5aa71daccc5
Natan PC <natanr123@gmail.com>
2018-12-17 14:11:10 +0200

    isolated agents

--- routes/agents.js
+++ routes/agents.js
@@ -1,21 +1,28 @@
 import express from 'express';
 import { promisify } from 'util';
 import models from './../models'
+const Sequelize = require('sequelize');
 
 const router = express.Router();
 
 const index = (req, res) => {
     // https://stackoverflow.com/questions/22627258/how-does-group-by-works-in-sequelize
-    models.agent.findAll().then((usersResponse) => {
-
-        const users = usersResponse.map((user) => {
+    const query = {
+        attributes: ['code', [Sequelize.fn('count', Sequelize.col('code')), 'missions_count']],
+        group: ['code'],
+        having: Sequelize.literal('count(code) = 1'),
+    };
+    models.agent.findAll(query)
+        .then((usersResponse) => {
+            const isolatedAgents = usersResponse.map((user) => {
             return user.get({
                 plain: true
-            })
+            }).code } );
+            return isolatedAgents })
+        .then((isolatedAgents) => {
+            console.log('ooooooooooo: ', isolatedAgents);
+            res.send(isolatedAgents);
         });
-        console.log(users);
-        res.send(users);
-    });
 
 
 };
