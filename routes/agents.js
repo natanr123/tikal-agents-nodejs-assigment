ad05f324c789cb60e2edfdfedad1ddc664b1d678
Natan PC <natanr123@gmail.com>
2018-12-17 13:08:37 +0200

    agents

--- routes/agents.js
+++ routes/agents.js
@@ -1,12 +1,22 @@
 import express from 'express';
 import { promisify } from 'util';
-import '../config/sequelize'
+import models from './../models'
 
 const router = express.Router();
 
 const index = (req, res) => {
-    console.log('start');
-    res.send('dddddddddddddddd');
+    models.agent.findAll().then((usersResponse) => {
+
+        const users = usersResponse.map((user) => {
+            return user.get({
+                plain: true
+            })
+        });
+        console.log(users);
+        res.send(users);
+    });
+
+
 };
 
 router.get('/',  index);
