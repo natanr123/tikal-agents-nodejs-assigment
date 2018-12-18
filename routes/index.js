5f1ec99073a5eebb5a88a2b52cd4bc21215952a6
Natan PC <natanr123@gmail.com>
2018-12-18 18:03:54 +0200

    order

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,37 @@
 import express from 'express';
 
+const Sequelize = require('sequelize');
 
+const models = require('./../models');
 
 const router = express.Router();
 
+const countriesByIsolation = (req, res) => {
 
+	models.mission
+		.findAll({
+			include:[models.agent],
+			attributes: [
+				'country',
+				[Sequelize.fn('count', Sequelize.col('*')), 'isolated_count'],
+
+			],
+			where: {'$agent.missions_count$': 1},
+			group: ['mission.country'],
+		})
+		.then((models) => {
+			return models.map((model) => {
+				const plain = model.get({plain: true});
+				return {country: plain.country, isolatedCount: plain.isolated_count} } );
+		})
+		.then((isolatedArr)=>{
+			// The order should have been done by sql but I had some issues with Sequelize
+			const orderd = isolatedArr.sort((a,b) => b.isolatedCount - a.isolatedCount)
+			res.send(orderd[0]);
+		})
+};
+
+router.get('/countries-by-isolation', countriesByIsolation);
 
 
 module.exports = router;
--- routes/missions.js
+++ routes/missions.js
@@ -7,40 +7,15 @@ const models = require('./../models');
 
 const router = express.Router();
 
-const index = (req, res) => {
-
-	// models.mission
-	// 	.findAll()
-	// 	.then((missions) => {
-	// 		const data = missions.map((user) => {
-	// 			return user.get({
-	// 				plain: true
-	// 			}) } );
-	// 		res.send(data);
-	//
-	// 	})
-
+function index(req, res) {
 	models.mission
-		.findAll({
-			include:[models.agent],
-			attributes: [
-				'country',
-				[Sequelize.fn('count', Sequelize.col('country')), 'isolated_count']
-			],
-
-			where: {'$agent.missions_count$': 1},
-			group: ['mission.country']
-		})
-		.then((models) => {
-			console.log(models);
-			const data = models.map((model) => {
-				return model.get({
-					plain: true
-				}) } );
+		.findAll()
+		.then((data) => {
 			res.send(data);
+		});
+}
 
-		})
-};
+
 
 router.get('/', index);
 
